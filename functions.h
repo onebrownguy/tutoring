232530e5c352d9cdfcf284f77255355e528ce502
onebrownguy <131419488+onebrownguy@users.noreply.github.com>
2024-06-16 10:55:38 -0700

    Add files via upload

---
+++ functions.h
@@ -0,0 +1,23 @@
+//
+//  functions.hpp
+//  
+//
+//  Created by Jenifer Maurer on 6/15/24.
+//
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::fstream;
+using std::string;
+
+int count_characters(string);
+void display_file(fstream&);
+
+#endif /* FUNCTIONS_H */
---
+++ main.h
@@ -0,0 +1,19 @@
+/*
+ * Replace this with your header file
+ */
+
+
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+#include <fstream>
+#include "functions.h"
+
+using std::cout;
+using std::endl;
+using std::fstream;
+
+
+#endif /* MAIN_H */
